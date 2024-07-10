db12da968633553af44babd83fd4753669459bb7
deky93 <31776613+deky93@users.noreply.github.com>
2024-07-10 23:44:09 +0200

    Delete src directory

--- src/app/shopping-list/store/shopping-list.actions.ts
+++
@@ -1,7 +0,0 @@
-import { Action } from "@ngrx/store";
-
-export const ADD_INGREDIENT = 'ADD_INGREDIENT';
-
-export class AddIngredient implements Action {
-    readonly type = ADD_INGREDIENT;
-}
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  fireBaseAPIKey:'AIzaSyC3zlNUNjDOeUUyGGxrzHL1UGO0ryWteqI'
-};
